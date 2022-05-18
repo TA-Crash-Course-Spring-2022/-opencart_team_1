bd081f5b87d5b2689f5936c57e35473b1e7f894b
Natalya <natalyatsudna@gmail.com>
2022-05-18 20:14:53 +0300

    add wish-list

---
+++ opencart/src/main/java/steps/WishListBL.java
@@ -0,0 +1,17 @@
+package steps;
+
+import containers.WishListContainer;
+import pages.WishList;
+import pages.containers.WishListContainer;
+
+
+public class WishListBL {
+
+    private WishList wishList;
+
+    public WishListBL wishListProductAddToCart(String productName){
+        WishListContainer product = wishList.getProducts().stream().filter(e -> e.productGetName().equalsIgnoreCase(productName)).findFirst().orElseThrow(NullPointerException::new);
+        product.wishListAddToCart().click();
+        return this;
+    }
+}
