e6fb612f561b8a4a3ca3ab6712cbe22be6a26d3f
NStrafer <saviaknazarii@gmail.com>
2022-05-19 14:39:11 +0300

    add AdminEditCurrencyPage

---
+++ opencart/src/main/java/pages/AdminEditCurrencyPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import lombok.Getter;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+@Getter
+public class AdminEditCurrencyPage extends BasePage {
+    @FindBy(id = "input-title")
+    private WebElement currencyTitleInput;
+
+    @FindBy(id = "input-code")
+    private WebElement codeInput;
+
+    @FindBy(xpath = " //div[@class='form-group required']//span")
+    private WebElement codeSpanAdditionalInfo;
+
+    @FindBy(id = "input-symbol-left")
+    private WebElement symbolLeftInput;
+
+    @FindBy(id = "input-symbol-right")
+    private WebElement symbolRightInput;
+
+    @FindBy(id = "input-decimal-place")
+    private WebElement decimalPlacesInput;
+
+    @FindBy(id = "input-decimal-place")
+    private WebElement valueInput;
+
+    @FindBy(xpath = "//div[@class='form-group']//span")
+    private WebElement valueSpanAdditionalInfo;
+
+    @FindBy(id = "input-status")
+    private Select statusSelect;
+
+    @FindBy(xpath = "//div[@class='pull-right']/button")
+    private WebElement saveEditCurrencyButton;
+
+    @FindBy(xpath = "//div[@class='pull-right']/a")
+    private WebElement cancelEditCurrencyButton;
+}
