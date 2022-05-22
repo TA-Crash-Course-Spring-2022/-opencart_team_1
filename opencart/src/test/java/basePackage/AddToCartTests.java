ff36eddabcd11efbe1c4b0fe0bd8a35d4a522c2b
Mary <dmutruk.maria4@gmail.com>
2022-05-22 19:31:46 +0300

    Add 2 products to shopping cart from main page

---
+++ opencart/src/test/java/basePackage/AddToCartTests.java
@@ -0,0 +1,19 @@
+package basePackage;
+
+import navigation.Navigation;
+import org.testng.annotations.Test;
+import steps.MainPageBL;
+
+import static enums.Url.BASIC_URL;
+
+public class AddToCartTests extends BaseTest {
+
+    @Test
+    public void addTwoProductsToCart() {
+        new Navigation().navigateToUrl(BASIC_URL.getUrlValue());
+        new MainPageBL()
+                .addProductToCart("iPhone")
+                .addProductToCart("MacBook")
+                .verifyAddToShoppingCart();
+    }
+}
